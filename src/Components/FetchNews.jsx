a9f52b4e6e7314499cb2014c5ca733d2f0cce0f8
Shubhanshu Kumar <kumarshubhanshu808@gmail.com>
2022-08-21 01:50:21 +0530

    Create fetchNews arrow function to get data from api news

--- src/Components/FetchNews.jsx
+++ src/Components/FetchNews.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import axios from "axios";
 export default function FetchNews() {
+ const fetchNews = () =>{
+   axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=1877fb5c99ff40c9a051ed08ece32067")
+   .then((response)=>{
+     console.log(response);
+   })
+  }
   return (
     <>
       <div className="container my-3">
         <div className="row">
           <div className="col-4">
-            <button className="btn btn-primary">FetchNews</button>
+            <button className="btn btn-primary" onClick={fetchNews}>FetchNews</button>
           </div>
         </div>
       </div>
