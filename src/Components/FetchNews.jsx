a8ffb31f97b382fea54332decf66ad0ea7d45ba0
Shubhanshu Kumar <kumarshubhanshu808@gmail.com>
2022-08-21 01:55:00 +0530

    Create a useState variable to store the get data

--- src/Components/FetchNews.jsx
+++ src/Components/FetchNews.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 export default function FetchNews() {
+  const [state , setState] = useState([])
  const fetchNews = () =>{
    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=1877fb5c99ff40c9a051ed08ece32067")
    .then((response)=>{
